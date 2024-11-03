d9d6c782eee6bf9eb3665fc51220c1bda94e5dae
fotistsiou <fotis.tsioumas@outlook.com>
2024-11-03 22:10:27 +0200

    Day 3

--- src/fotistsiou/hyperskill/java/day_3/Main.java
+++ src/fotistsiou/hyperskill/java/day_3/Main.java
@@ -25,20 +25,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("You are an indie java game developer looking to capitalize on the upcoming Halloween hype.");
-        System.out.println("For your game, you're designing a HauntedMansion class with various room types.");
-        System.out.println("Which combination of access modifiers would be most appropriate for the following elements?");
-        System.out.println("- The HauntedMansion class itself");
-        System.out.println("- A GhostRoom inner class used only within HauntedMansion");
-        System.out.println("- A method to add new rooms, used by other classes");
-        System.out.println("- A list storing all rooms, accessed only within HauntedMansion");
-        System.out.println("Select one option from the list:");
-        System.out.println("a. public, private, public, private");
-        System.out.println("b. public, protected, public, package-private");
-        System.out.println("c. public, private, private, protected");
-        System.out.println("d. package-private, private, public, private");
-        System.out.println("e. package-private, public, public, private");
-
         while (true) {
             System.out.println("Enter a character from 'a' to 'e':");
             String input = scanner.next();
